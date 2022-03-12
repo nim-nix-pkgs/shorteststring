3e1dd6ce0d82f06fbd920e8c4d596d2be004a123
hugosenari <hugosenari@gmail.com>
2022-03-12 04:07:12 -0300

    chore: re index shorteststring

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''word size strings stored in an integer'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-shorteststring-master.flake = false;
+  inputs.src-shorteststring-master.owner = "metagn";
+  inputs.src-shorteststring-master.ref   = "refs/heads/master";
+  inputs.src-shorteststring-master.repo  = "shorteststring";
+  inputs.src-shorteststring-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-shorteststring-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-shorteststring-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
