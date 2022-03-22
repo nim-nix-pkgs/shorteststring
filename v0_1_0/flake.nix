c0703a19b4845ec9961c553843698de0fd41aa84
hugosenari <hugosenari@gmail.com>
2022-03-22 01:16:43 -0300

    chore: re-index ../nimpkgs/s/shorteststring

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-shorteststring-v0_1_0.flake = false;
+  inputs.src-shorteststring-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-shorteststring-v0_1_0.owner = "metagn";
-  inputs.src-shorteststring-v0_1_0.ref   = "v0_1_0";
   inputs.src-shorteststring-v0_1_0.repo  = "shorteststring";
   inputs.src-shorteststring-v0_1_0.type  = "github";
   
